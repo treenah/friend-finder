f7fad87d74bc460ecfb6c106ddb0153dbbc57587
treenah <tsida08@gmail.com>
2019-03-20 22:55:08 -0700

    clean up a bit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,12 @@
-var path = require('path');
-
 var path = require("path");
 
 module.exports = function(app){
 
-	app.use('/home', function (req, res) {
-		res.sendFile(path.join(__dirname, "home.html"));
+	app.use('/survey', function (req, res) {
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	});
 
-	app.get('/survey', function (req, res) {
-	  	res.sendFile(path.join(__dirname, "survey.html"));
+	app.get('*', function (req, res) {
+	  	res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
 };
--- server.js
+++ server.js
@@ -3,17 +3,15 @@ var bodyParser = require("body-parser");
 
 // ==============================Set up the Express App========================================
 
-var app = express();
+var app = express(__dirname + "/home.html");
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 
-app.use(express.static(__dirname + "/app/public"));
-app.use(express.static('../client'));
-app.use(bodyParser.json());
+
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 // ==============================Require routing========================================
 
@@ -23,5 +21,5 @@ require("./app/routing/htmlRoutes")(app);
 // =========================Starts the server to begin listening================================
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("howdy, pa'nah'! " + PORT);
 });
